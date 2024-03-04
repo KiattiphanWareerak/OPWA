1237295f8bb4647d0fb152ef0510c71fc1f9d1bc
visarutford <visarut.ki@kkumail.com>
2024-03-04 23:07:12 +0700

    convert barrel

--- public/scripts/sgpOil.js
+++ public/scripts/sgpOil.js
@@ -1,38 +1,67 @@
 const getData = async () => {
-    const res = await fetch("http://localhost:3002/getSgdOilPrice"); // เปลี่ยน URL ที่นี่
-    const jsonData = await res.json();
-    const cdata = jsonData.resultData.map((data) => {
-      const { oilsgd_time, oilsgd_value } = data; // ปรับการเข้าถึงข้อมูลใหม่ที่ต้องการ
-      const timeParts = oilsgd_time.split(':');
-      const datetime = new Date().setHours(parseInt(timeParts[0]), parseInt(timeParts[1]), parseInt(timeParts[2]), 0); // สร้างวันที่ใหม่จากเวลาที่ให้มา
-      console.log("datetime = ",datetime)
-      return {
-        time: datetime,
-        value: parseFloat(oilsgd_value),
-      };
-    });
-    console.log("cdata = ", cdata);
-    return cdata;
-  };
-  
+  const res = await fetch("http://localhost:3002/getSgdOilPrice"); // เปลี่ยน URL ที่นี่
+  const jsonData = await res.json();
+  const cdata = jsonData.resultData.map((data) => {
+    const { oilsgd_time, oilsgd_value } = data; // ปรับการเข้าถึงข้อมูลใหม่ที่ต้องการ
+    const timeParts = oilsgd_time.split(":");
+    const datetime = new Date().setHours(
+      parseInt(timeParts[0]),
+      parseInt(timeParts[1]),
+      parseInt(timeParts[2]),
+      0
+    ); // สร้างวันที่ใหม่จากเวลาที่ให้มา
+    console.log("datetime = ", datetime);
+    return {
+      time: datetime,
+      value: parseFloat(oilsgd_value),
+    };
+  });
+  console.log("cdata = ", cdata);
+  return cdata;
+};
 
 const displayChart = async () => {
-    const chartProperties = {
-      width: 1400,
-      height: 600,
-      timeScale: {
-        timeVisible: true,
-        secondsVisible: true,
-      },
-    };
-  
-    const domElement = document.getElementById("oilChart");
-    const chart = LightweightCharts.createChart(domElement, chartProperties);
-    const lineSeries = chart.addLineSeries();
-    const linedata = await getData();
-    console.log("linedata = ",linedata)
-    
-    lineSeries.setData(linedata);
+  const chartProperties = {
+    width: 1400,
+    height: 600,
+    timeScale: {
+      timeVisible: true,
+      secondsVisible: true,
+    },
   };
-  
-  displayChart();
+
+  const domElement = document.getElementById("oilChart");
+  const chart = LightweightCharts.createChart(domElement, chartProperties);
+  const lineSeries = chart.addLineSeries();
+  const linedata = await getData();
+  console.log("linedata = ", linedata);
+
+  lineSeries.setData(linedata);
+};
+
+const convertBtn = document.getElementById("convert-value");
+convertBtn.addEventListener("click", convertAndDisplayResult);
+
+function convertAndDisplayResult() {
+  const barrelInput = document.getElementById("barrel");
+  const barrelValue = barrelInput.value;
+
+  const apiUrl = `http://localhost:3002/getConvertBarreltoLiter?barrel=${barrelValue}`;
+
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const literInput = document.getElementById("liter");
+      literInput.value = data;
+    })
+    .catch((error) => {
+      console.error("Error:", error.message);
+    });
+}
+
+displayChart();
